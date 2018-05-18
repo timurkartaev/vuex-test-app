4c15b11732a6039ce1766943033e2ccf28cdf189
timurkartaev <timurkartaev@gmail.com>
2018-05-18 18:27:32 +0000

    added vuex and store

---
+++ src/CounterControllers.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Counter Controllers</p>
+        <button @click.prevent="incrementCounter">Increment</button>
+        <button @click.prevent="decrementCounter">Decrement</button>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        methods: {
+            incrementCounter() {
+                this.$store.state.counter++;
+            },
+            decrementCounter() {
+                this.$store.state.counter--;
+            }
+        }
+    }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {store} from './store/store';
+
+
 new Vue({
   el: '#app',
+  store: store,  // could be just store
   render: h => h(App)
 })
